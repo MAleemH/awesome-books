5b7f8d381e1b97c6a6e9d5e0de9178830ffa2306
Muhammad Aleem Hashmi <aleemhashmi4321@gmail.com>
2023-01-10 19:58:44 +0500

    removed JS error

--- practice.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector('#form');
-const bookList = document.querySelector('.book-list');
-const titleInput = document.querySelector('#input_title');
-const authorInput = document.querySelector('#input_author');
-const addBtn = document.querySelector('#add_btn');
-
-const collections = JSON.parse(localStorage.getItem('collections')) || [];
-
-class 

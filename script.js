9515085207ad5a2a5516ca92efe986c3adb778fe
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-01-11 16:27:04 +0630

    use js DOM to change the content display and add footer section for copyright

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 import Books from './modules/book.js';
 
-// menu buttons
-const booksListView = document.querySelector('#books-list-view');
-const addNewBookView = document.querySelector('#add-book-view');
-const contactView = document.querySelector('#contact-us-view');
 // menu buttons closed
 const form = document.querySelector('#form');
 const bookList = document.querySelector('.book-list');
@@ -72,8 +68,39 @@ window.onload = () => {
 };
 
 // calling all views
+// menu buttons
+const booksListView = document.querySelector('#books-list-view');
+const addNewBookView = document.querySelector('#add-book-view');
+const contactView = document.querySelector('#contact-us-view');
 
-contactView.addEventListener('click', () => {
-  document.querySelector('#contact-us').style.display = 'block';
-  document.querySelector('#books-list').style.display = 'none';
-})
+// calling section
+const listbook = document.querySelector('#books-list');
+const addbook = document.querySelector('#add-book');
+const contactus = document.querySelector('#contact-us');
+
+booksListView.onclick = () => {
+  booksListView.classList.add('active');
+  contactView.classList.remove('active');
+  addNewBookView.classList.remove('active');
+  listbook.style.display = 'block';
+  contactus.style.display = 'none';
+  addbook.style.display = 'none';
+};
+
+contactView.onclick = () => {
+  contactView.classList.add('active');
+  booksListView.classList.remove('active');
+  addNewBookView.classList.remove('active');
+  contactus.style.display = 'block';
+  addbook.style.display = 'none';
+  listbook.style.display = 'none';
+};
+
+addNewBookView.onclick = () => {
+  addNewBookView.classList.add('active');
+  booksListView.classList.remove('active');
+  contactView.classList.remove('active');
+  addbook.style.display = 'block';
+  contactus.style.display = 'none';
+  listbook.style.display = 'none';
+};
